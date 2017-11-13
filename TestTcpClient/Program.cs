f765d84ba93b0afcf7122b2221e75b7ba7682912
rajwilkhu <raj.wilkhu@tesco.com>
2017-11-13 15:13:28 +0000

    Target .NET Framework 4.5

--- TestTcpClient/Program.cs
+++ TestTcpClient/Program.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
 using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net.Sockets;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace TestTcpClient
 {
